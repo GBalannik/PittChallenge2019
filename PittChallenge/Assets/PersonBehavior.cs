28fe4be4de9d0d620024ad2aadce19b6cef89d33
GBalannik <gib24@pitt.edu>
2019-10-12 00:13:59 -0400

    Unity Game File

---
+++ PittChallenge/Assets/PersonBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float sickness;
+    public float infectivity;
+    public float closeness;
+    public int age;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        age = Random.Range(25, 65);
+        infectivity = Random.Range(.1f, 1f);
+        closeness = Random.Range(.1f, 1f);
+        sickness = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
